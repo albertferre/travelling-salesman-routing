1f4c38670a64c09c85a329e0f4f16c3b82274b08
Albert Ferré Vidal <albertferrevidal@gmail.com>
2023-07-23 10:52:48 +0200

    black formatting

--- src/routing.py
+++ src/routing.py
@@ -2,11 +2,12 @@ from __future__ import print_function
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
+
 def print_solution(manager, routing, solution, names=None):
     """Prints solution on console."""
     # print('Objective: {} km'.format(int(solution.ObjectiveValue()/1000)))
     index = routing.Start(0)
-    plan_output = 'Route for vehicle:\n'
+    plan_output = "Route for vehicle:\n"
     route_distance = 0
     while not routing.IsEnd(index):
         idx = manager.IndexToNode(index)
@@ -14,7 +15,7 @@ def print_solution(manager, routing, solution, names=None):
             print_value = names[idx]
         else:
             print_value = idx
-        plan_output += ' {} ->'.format(print_value)
+        plan_output += " {} ->".format(print_value)
         previous_index = index
         index = solution.Value(routing.NextVar(index))
         route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
@@ -24,8 +25,8 @@ def print_solution(manager, routing, solution, names=None):
         print_value = names[idx]
     else:
         print_value = idx
-    plan_output += ' {}\n'.format(print_value)
-    plan_output += 'Route distance: {:.1f}km\n'.format(route_distance/1000)
+    plan_output += " {}\n".format(print_value)
+    plan_output += "Route distance: {:.1f}km\n".format(route_distance / 1000)
     print(plan_output)
 
 
@@ -33,11 +34,12 @@ def optimize_routes(distance_matrix, depot, names=None):
     """Entry point of the program."""
 
     # Create the routing index manager.
-    manager = pywrapcp.RoutingIndexManager(len(distance_matrix), 1, depot) # num of vehicles set to 1
+    manager = pywrapcp.RoutingIndexManager(
+        len(distance_matrix), 1, depot
+    )  # num of vehicles set to 1
 
     # Create Routing Model.
     routing = pywrapcp.RoutingModel(manager)
-
 
     def distance_callback(from_index, to_index):
         """Returns the distance between the two nodes."""
@@ -54,14 +56,15 @@ def optimize_routes(distance_matrix, depot, names=None):
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
     search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
 
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
 
     # Print solution on console.
     if solution:
-        print_solution(manager, routing, solution,names)
+        print_solution(manager, routing, solution, names)
         route = []
         index = routing.Start(0)
         while not routing.IsEnd(index):
